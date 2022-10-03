fc57d6ac3633407f5c249dec720866b8db8fdf00
kyoichi-taniguchi <taniguti.9401@icloud.com>
2022-10-03 14:20:23 +0900

    apiから観光施設情報を取得してtableviewに表示

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'amusement-facilities-app-ios' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for amusement-facilities-app-ios
+
+end
--- amusement-facilities-app/AmusementFacilitiesViewController.swift
+++ amusement-facilities-app/AmusementFacilitiesViewController.swift
@@ -8,27 +8,92 @@
 import Foundation
 import UIKit
 
+
+
 class AmusementFacilitiesViewController: UIViewController {
     
     @IBOutlet private weak var tableView: UITableView!
     
-    private let items: [String] = ["a", "b"]
+    private var amusementFacilities: [AmusementFacilitiesResponse] = [] {
+        didSet {
+            print("set")
+            print(amusementFacilities)
+            DispatchQueue.main.async {
+                //print(jsonResponse.Item)
+                self.tableView.reloadData()
+                
+            }
+            //tableView.reloadData()
+            
+        }
+    }
+    
+    private let items: [String] = []
+    
+    func unicodeDecode(beforeText: String) -> String {
+        let wI = NSMutableString( string: beforeText )
+        CFStringTransform( wI, nil, "Any-Hex/Java" as NSString, true )
+        return wI as String
+    }
+    
+    private let recs: [String] = ["oidghjafsnlaisjkdf", "ああぢｊｈで"]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let url: URL = URL(string: "https://t8lej63df8.execute-api.ap-northeast-1.amazonaws.com/amusement-facilities-stage/amusement-facilities")!
+        let task: URLSessionTask = URLSession.shared.dataTask(with: url, completionHandler: {(data, response, error) in
+            if let data = data {
+                
+                let str = String(data: data, encoding: .utf8) ?? ""
+                let jsonData = self.unicodeDecode(beforeText: str).data(using: .utf8)!
+                let jsonResponse = try! JSONDecoder().decode(JsonResponse.self, from: jsonData)
+                
+                self.amusementFacilities = [jsonResponse.Item]
+                
+            }
+        })
+        task.resume()
+        
         tableView.dataSource = self
+        
     }
-    
 }
 
 extension AmusementFacilitiesViewController: UITableViewDataSource {
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return amusementFacilities.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ??
-                        UITableViewCell(style: .default, reuseIdentifier: "cell")
-            cell.textLabel?.text = items[indexPath.row]
-            return cell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "amusementFacilitiesTableViewCell", for: indexPath) as? AmusementFacilitiesTableViewCell else {
+            fatalError("AmusementFacilitiesTableViewCell is not found")
+        }
+        cell.nameLable!.text = amusementFacilities[indexPath.row].name
+        cell.recommendationLabel.text = amusementFacilities[indexPath.row].recommendation
+        return cell
     }
+}
+
+class AmusementFacilitiesTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var recommendationLabel: UILabel!
+    
+    
+}
+
+struct JsonResponse: Decodable {
+    var Item: AmusementFacilitiesResponse
+}
+ 
+struct AmusementFacilitiesResponse: Decodable {
+    var placeId: String
+    var name: String
+    var location: String
+    var price: String
+    var recommendation: String
+    //var pictureUrls: [String?]
+    //var tag: [String]
 }
